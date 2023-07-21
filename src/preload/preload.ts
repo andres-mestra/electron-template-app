6003f02efcff719ca7a05c28aa2d64daac4653c3
andresmestradev <andresmestra11a@gmail.com>
2023-07-21 10:15:35 -0500

    feat: define preload methods

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,3 +1,35 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-console.log('preload.js')
+import { contextBridge, ipcRenderer } from 'electron'
+
+type Callback = <TArgs>(...args: TArgs[]) => void
+type Listener = <TArgs>(
+  event: Electron.IpcRendererEvent,
+  ...args: TArgs[]
+) => void
+
+const methods = {
+  ipcRenderer: {
+    send<TData>(channel: string, data?: TData) {
+      ipcRenderer.send(channel, data)
+    },
+    on(channel: string, func: Callback) {
+      const listener: Listener = (event, ...args) => func(...args)
+      ipcRenderer.on(channel, listener)
+      return listener
+    },
+    once(channel: string, func: Callback) {
+      const listener: Listener = (event, ...args) => func(...args)
+      ipcRenderer.once(channel, listener)
+      return listener
+    },
+    invoke: <TReturn, TData>(channel: string, data?: TData) => {
+      return ipcRenderer.invoke(channel, data) as Promise<TReturn>
+    },
+    removeListener(channel: string, listener: Listener) {
+      ipcRenderer.removeListener(channel, listener)
+    },
+  },
+}
+
+contextBridge.exposeInMainWorld('electron', methods)
+
+export type IpcRendererType = typeof methods
